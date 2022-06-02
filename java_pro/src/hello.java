ec97e379f3a2d36f41e0da16a81087d4b73c5dd1
韩少京 <490349175@qq.com>
2022-06-03 00:11:25 +0800

    Initial commit

---
+++ java_pro/src/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.printf("hhh");
+        System.out.printf("hhh");
+        System.out.printf("hhh");
+        System.out.printf("hhh");
+        System.out.printf("hhh");
+
+
+    }
+}
